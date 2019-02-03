ba0933c3904ffe45e039c30244ddc425a244b732
Abdelkrim Meziani <ramziabdelkrim@gmail.com>
2019-02-03 20:47:21 +0100

    Crypt text script first commit

---
+++ crypt_text.py
@@ -0,0 +1,23 @@
+
+
+def crypt_text(mytext):
+
+	key = 2#input("Enter your Key :")
+	mylst = []
+	mode = "Encrypt" # or Decrypt
+
+	for x in range(0,len(mytext)):
+
+		if mode == "Encrypt":
+			n_letter = ord(mytext[x])+int(key)
+			c_item = chr(n_letter)
+			mylst.append(c_item)
+
+		if mode == "Decrypt":
+			n_letter = ord(mytext[x])-int(key)
+			ch = chr(n_letter)
+			mylst.append(ch)
+
+	return "".join(mylst)
+
+print(crypt_text("""Once you stop learning, you start dying."""))
